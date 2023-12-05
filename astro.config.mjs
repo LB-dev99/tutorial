3fba80cdac9a83fefd50b8580aadd132268cbed6
MimmoLB~ <seaghindosh@gmail.com~>
2023-12-05 11:52:11 +0100

    Basic Fixes and adjustments

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import netlify from "@astrojs/netlify/functions";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: 'https://radiant-macaron-402dc3.netlify.app/',
+  integrations: [tailwind(), preact()],
   output: "server",
   adapter: netlify()
 });
